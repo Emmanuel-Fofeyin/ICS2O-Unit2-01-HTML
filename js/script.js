66f88e20c8ff2e03318b4a9ab87f8f510e3600f9
Emmanuel Fofeyin <99355350+Emmanuel-Fofeyin@users.noreply.github.com>
2022-03-11 05:12:03 +0000

    fixed errors

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// created my Emmanuel-Fofeyin
+// Created on March 10 2022
+// This file contains the JS instructions for index.html
+
+function myButtonClicked() {
+  // this function displays "Hello, World!"
+  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+}
